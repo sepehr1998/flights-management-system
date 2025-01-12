cfe3916d501c86ea5ec9ccd7cddd534390adebf9
Sepehr Samadi <samadisepehr@gmail.com>
2025-01-12 10:55:34 +0200

    font awesome installed. new search flight route added

--- flights-management-client/src/app/app.routes.ts
+++ flights-management-client/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SearchFlightsComponent } from "./search-flights/search-flights.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SearchFlightsComponent }
+];
