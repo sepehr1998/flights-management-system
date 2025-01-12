0817b88e1b5700fc4304156c167cc54659fa8774
Sepehr Samadi <samadisepehr@gmail.com>
2025-01-12 10:56:20 +0200

    new search flight component created

---
+++ flights-management-client/src/app/search-flights/search-flights.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-flights',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-flights.component.html',
+  styleUrl: './search-flights.component.css'
+})
+export class SearchFlightsComponent {
+
+}
