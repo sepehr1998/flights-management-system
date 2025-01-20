f581166214112d912cd5f92bd5401acd1c1baa79
Sepehr Samadi <samadisepehr@gmail.com>
2025-01-20 18:16:44 +0200

    Swagger and OpenAPI server configured

--- FlightsManagementBackend/FlightsManagementBackend/Program.cs
+++ FlightsManagementBackend/FlightsManagementBackend/Program.cs
@@ -1,11 +1,21 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddServer(new OpenApiServer
+    {
+        Description = "Development Server",
+        Url = "https://localhost:7241"
+    });
+});
 
 var app = builder.Build();
 
 app.MapControllers();
-
+app.UseSwagger().UseSwaggerUI();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
