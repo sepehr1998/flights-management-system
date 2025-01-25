ceab100d62ae3c706b92915fefc4a03066dc33cc
Sepehr Samadi <samadisepehr@gmail.com>
2025-01-25 20:33:30 +0200

    register passenger form component developed [1]: form elements created [2]: data binding done

---
+++ flights-management-client/src/app/register-passenger/register-passenger.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { PassengerService } from "../api/services/passenger.service";
+import {FormsModule, ReactiveFormsModule, FormBuilder, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-register-passenger',
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './register-passenger.component.html',
+  standalone: true,
+  styleUrl: './register-passenger.component.css'
+})
+export class RegisterPassengerComponent {
+  constructor(private passengerService: PassengerService, private fb: FormBuilder) {}
+
+  form = this.fb.group({
+    email: [''],
+    firstName: [''],
+    lastName: [''],
+    isFemale: [true]
+  })
+
+  register() {
+    this.passengerService.registerPassenger();
+  }
+}
