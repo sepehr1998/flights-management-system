99ad3793f2402d5f2469661f5435d3793ac31644
Sepehr Samadi <samadisepehr@gmail.com>
2025-01-30 06:29:05 +0200

    validation added to new passenger dto

--- FlightsManagementBackend/FlightsManagementBackend/Dtos/NewPassengerDto.cs
+++ FlightsManagementBackend/FlightsManagementBackend/Dtos/NewPassengerDto.cs
@@ -1,8 +1,9 @@
+using System.ComponentModel.DataAnnotations;
 namespace FlightsManagementBackend.Dtos;
 
 public record NewPassengerDto(
-    string Email,
-    string FirstName,
-    string LastName,
-    bool Gender
+    [Required][EmailAddress][StringLength(100, MinimumLength = 3)] string Email,
+    [Required][MinLength(2), MaxLength(35)] string FirstName,
+    [Required][MinLength(2), MaxLength(35)] string LastName,
+    [Required] bool Gender
 );
