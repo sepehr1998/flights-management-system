8c3d7575fabf9f188891bebb81546a380565b67b
Sepehr Samadi <samadisepehr@gmail.com>
2025-02-05 18:58:35 +0200

    redirect to login if user not existing for my booking

--- flights-management-client/src/app/my-bookings/my-bookings.component.ts
+++ flights-management-client/src/app/my-bookings/my-bookings.component.ts
@@ -4,6 +4,7 @@ import { BookingService } from "../api/services/booking.service";
 import { AuthService } from "../auth/auth.service";
 import {FormBuilder} from "@angular/forms";
 import {DatePipe} from "@angular/common";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-my-bookings',
@@ -16,8 +17,14 @@ import {DatePipe} from "@angular/common";
 })
 export class MyBookingsComponent implements OnInit{
   bookings!: BookingRm[];
-  constructor(private bookingService: BookingService, private authService: AuthService) {}
+  constructor(
+    private bookingService: BookingService,
+    private authService: AuthService,
+    private router: Router) {}
   ngOnInit(): void {
+    if(!this.authService.currentUser?.email)
+      this.router.navigate(['register-passenger'])
+
     this.bookingService.listBooking({ email: this.authService.currentUser?.email ?? '' })
       .subscribe(r=>this.bookings = r, this.handleError);
   }
